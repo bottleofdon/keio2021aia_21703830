0e1627682c0da1654df4809b6b732218c4d59f93
Yotaro <enomoto.yotaro@gmail.com>
2021-06-03 17:06:41 +0900

    Sonar_data

--- assignment1/keio2021aia_21703830_assignment1/sonar/sonar_data.py
+++ assignment1/keio2021aia_21703830_assignment1/sonar/sonar_data.py
@@ -1,27 +1,40 @@
 from . import *
 
+
 class Sonar_Data:
 
-    def __init__(self, data_file_path='', data_file_name='sonar_data.pkl'):
-                
+    def __init__(self, data_file_path='../data/', data_file_name='sonar_data.pkl'):
+
         self.data = []
 
-        YOUR_CODE
+        filepath = data_file_path+data_file_name
+
+        with open(filepath, 'rb') as f:
+            dat = pkl.load(f)
+
+        k = ["r", "m"]
+
+        for i in k:
+            for j in dat[i]:
+                if i == "r":
+
+                    self.data.append([j, 0])
+                else:
+                    self.data.append([j, 1])
 
         self.shuffle()
 
     def __iter__(self):
-        
         return self
 
     def __next__(self):
-        
-        YOUR_CODE
+
+        return self.data
 
     def shuffle(self):
-        
+
         random.shuffle(self.data)
-    
+
     def __len__(self):
-        
+
         return len(self.data)
