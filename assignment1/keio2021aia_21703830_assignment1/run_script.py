4052d1cec73a8e3a7e790a604cd3be6f08d6289e
Yotaro <enomoto.yotaro@gmail.com>
2021-06-03 17:49:50 +0900

    Sonar_Model

--- assignment1/keio2021aia_21703830_assignment1/run_script.py
+++ assignment1/keio2021aia_21703830_assignment1/run_script.py
@@ -1,29 +1,37 @@
 ##
-from sonar.sonar_data import * 
-from sonar.sonar_model import * 
-from sonar.sonar_trainer import * 
+from sonar.sonar_data import *
+from sonar.sonar_model import *
+from sonar.sonar_trainer import *
 from cats.cat_data import *
 from cats.cat_model import *
 from cats.cat_trainer import *
 ##
 
 ##
+
+
 def main():
 
-    data = YOUR_CODE
-    model = YOUR_CODE
-    trainer = YOUR_CODE
-    costs, accuracies = YOUR_CODE
+    Data = Sonar_Data()
+
+    model = Sonar_Model(dimension=len(
+        [next(iter(Data))][0][0][0]), activation=perceptron)
+
+    trainer = Sonar_Trainer(next(iter(Data)), model)
+
+    costs, accuracies = trainer.train(0.1, 500)
+
     model.save_model()
 
-    data = YOUR_CODE
-    model = YOUR_CODE
-    trainer = YOUR_CODE
-    costs, accuracies = YOUR_CODE
+    Data = Cat_Data()
+    model = Cat_Model(dimension=len(
+        [next(iter(Data))][0][0][0]), activation=sigmoid)
+    trainer = Cat_Trainer(Data, model)
+    costs, accuracies = trainer.train(0.1, 80)
     model.save_model()
 ##
+
 
 ##
 if __name__ == "__main__":
     main()
-##
